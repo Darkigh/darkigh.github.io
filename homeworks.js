e1e9220f0649888f15a5a4e26e4696c1813fae0b
Taha Mohamed <164627988+Darkigh@users.noreply.github.com>
2026-05-08 21:44:16 +0300

    Add new homework entries to homeworks.js

--- homeworks.js
+++ homeworks.js
@@ -18,5 +18,9 @@ window.HOMEWORKS = [
   { id: 4, title: "HW #4", desc: "Answer questions",           file: "HW4.docx"    },
   { id: 5, title: "HW #5", desc: ".",                          file: "HW5.docx"   },
   { id: 6, title: "HW #6", desc: ".",                          file: "HW6.pdf"   },
-  { id: 7, title: "HW #7", desc: ".",                          file: "HW7.pdf"   }
+  { id: 7, title: "HW #7", desc: ".",                          file: "HW7.pdf"   },
+  { id: 8, title: "HW #8P1", desc: "movie website",                          file: "HW8P1.html" },
+  { id: 9, title: "HW #8P2", desc: ".",                          file: "HW8P2.pdf"   },
+  { id: 10, title: "HW #8P3", desc: ".",                          file: "HW8P3.pdf"   },
+  { id: 11, title: "HW #9", desc: ".",                          file: "HW9.html"   },
 ];
